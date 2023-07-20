13b9a2b2f863ff165429445f31d339659a8be328
Rishi Singh <rs3949427@gmail.com>
2023-07-21 01:33:50 +0530

    created home page, login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import Home from './pages/home/Home.jsx';
-function App() {
-  const [count, setCount] = useState(0)
+import Login from './pages/login/Login.jsx';
+import Lobby from './pages/lobby/Lobby.jsx';
+import { Routes, Route } from "react-router-dom"
 
+function App() {
   return (
     <>
-     <Home />
+      <div className='dark:bg-slate-800 h-screen w-screen justify-items-center justify-center flex'>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="login" element={ <Login/> } />
+          <Route path="lobby" element={ <Lobby/> } />
+        </Routes>
+      </div>
     </>
   )
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,24 +2,22 @@ import React, { useState } from 'react'
 import './home.css';
 import Lobby from '../lobby/Lobby.jsx';
 import Login from '../login/Login.jsx';
+import { Link } from "react-router-dom";
+
 
 import './home.css';
 const Home = () => {
-    const [lobby, setLobby] = useState(false);
-
-    const [login, setLogin] = useState(false);
   return (
     <>
-      { !lobby && !login && <div className="container dark:bg-slate-950">
-        <button onClick={()=>setLobby(true)} className="guest">
-          Guest
-        </button>
-        <button onClick={()=>setLogin(true)} className="login">
-          Login/Sign-up
-        </button>
-      </div>}
-      {login && <Login />}
-      {lobby && <Lobby />}
+      <div className="dark:bg-slate-900 h-[600px] w-[700px] justify-center justify-items-center m-auto rounded-3xl">
+        <h1 className='text-white font-bold text-6xl m-auto flex justify-center mt-24'>Speed-O-Typing</h1>
+        <Link to = "/lobby" className="w-[70%] m-auto mt-12 py-7 px-20 flex justify-center bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Play as Guest
+        </Link>
+        <Link to = "/login" className="w-[70%] m-auto flex mt-8 py-7 px-20 justify-center bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Sign-in/Sign-up
+        </Link>
+      </div>
     </>
   )
 }
